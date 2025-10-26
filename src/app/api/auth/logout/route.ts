4760546d66477c64c84b39f9a7eb0fa31bc2faaa
amrhe <amr.hassan.emam@gmail.com>
2025-10-26 16:15:43 +0300

    improve logout action

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,9 +1,24 @@
 // app/api/logout/route.ts
+import { shiftStatus } from "@/models/enum.constants";
+import { Shift } from "@/models/Shift";
+import { User } from "@/models/User";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST() {
   const cookieStore = await cookies();
+  const userId = cookieStore.get("userId")?.value;
+
+  await Shift.findOneAndUpdate(
+    {userId, status: shiftStatus.IN_PROGRESS}, 
+    {status: shiftStatus.ENDED, endTime: Date.now()},
+    {new: true}
+  );
+
+  const user = await User.findById(userId);
+  user.isOnShift = false;
+  await user.save();
+  
   cookieStore.delete("access_token");
   cookieStore.delete("role");
   cookieStore.delete("email");
